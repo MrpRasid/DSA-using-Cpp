201a0a0d1e8f07ea09cb1d396a3591cdeab2ed84
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-09-06 02:49:19 +0530

    character type conversion
    character and int are converted into each

---
+++ type_casting/char_type_casting.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // character conversion by adding number
+    char ch='A';
+    char chh = ch+1;
+    cout<<chh <<endl;
+
+    //conversion int to char
+
+    int a =99;
+    char ch1 = a;
+    cout<<ch1 <<endl;
+
+    // conversion char to int 
+
+    char ch2 = 'g';
+    int num = ch2;
+    cout<<num;
+}
