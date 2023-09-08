32e72f83cb1747d4b141a6680c51b14d10a51fa9
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-09-08 19:12:05 +0530

    double the input
    multiply the input element and giv the output

---
+++ arrays/doubleArr_element.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10] ;
+    int n=10;
+    for(int i=0;i<n;i++){
+        cout<<"Enter the value for index: " << i <<endl;
+        cin>>arr[i];
+    }
+    
+    cout<<"The out put are : ";
+
+    for(int i=0;i<n;i++){
+        int num = arr[i] *2;
+        cout<<num <<endl;
+    }
+}
