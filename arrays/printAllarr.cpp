cb50e1051d906268f0782dc8b63b589ac6a669c2
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-09-08 18:45:12 +0530

    printing array
    printing all the arrays element using for loop

---
+++ arrays/printAllarr.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int ars[5] = {6,7,2,9,4};
+    // acceasing all arrays using for loop
+
+    for(int i=0;i<5;i++){
+        cout<<ars[i]<<" ";
+    }
+}
