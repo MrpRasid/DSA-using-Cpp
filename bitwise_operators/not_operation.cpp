c09f3bdb53dfce648336b986818df75bd27567f7
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-09-13 20:31:21 +0530

    not operation
    n not is -(n+1)

---
+++ bitwise_operators/not_operation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // not operation
+    // not answer will be let Number is n then : -(n+1);
+    int a;
+    cout<<"Enter a number for not operation: ";
+    cin>>a;
+    cout<<"Not is: "<<(~a);
+}
