4e1b1915adf319b6435994c8edac532b77e4bc9a
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-09-13 22:20:24 +0530

    unsign int

---
+++ bitwise_operators/unsign_int.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for unsigned value this return a large number value
+
+    unsigned int num =-5;
+
+    int bit =1;
+    cout<<(num<<bit)<<endl;
+
+    // for signed -ve value compile handel the value.
+
+    int a =-5;
+    cout<<(a>>1);
+
+}
