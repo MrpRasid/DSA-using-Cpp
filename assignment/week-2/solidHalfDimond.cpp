30a165d309f9c7afdf54594a1a03f219b826366d
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-10-10 01:57:03 +0530

    solid half dimond

---
+++ assignment/week-2/solidHalfDimond.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void pyramid(int n){
+
+    for(int i=0; i<2*n-1;i++){
+        int con = 0;
+
+        if(i<n){
+            con = i;
+        }
+        else{
+            con = n- (i%n)-2;
+        }
+        for(int j=0;j<=con;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+
+    cout<<"Enbter the number: ";
+    cin>>n;
+
+    pyramid(n);
+}
