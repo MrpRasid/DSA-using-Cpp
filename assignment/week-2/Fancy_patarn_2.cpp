9b4807e3b3adb465c4dcc406dc8a7672258f6697
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-10-10 04:25:36 +0530

    Fancy patarn two

---
+++ assignment/week-2/Fancy_patarn_2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num=1;
+    cout<<"enter the number: ";
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        // int logic=;
+       
+        for(int j=0;j<=i;j++){
+            cout<<num;
+            num++;
+        }
+
+        
+        cout<<endl;
+    }
+
+    // shrinking value
+    int start = num-n;
+
+    for(int i=0;i<n;i++){
+        int k = start;
+
+        for(int j=0;j<n-i;j++){
+            cout<<k;
+            k++;
+        }
+        start = start- (n-i-1);
+        cout<<endl;
+    }
+}
