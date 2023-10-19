d5c6ed23e93b301208ff64de75da46ff04b79260
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-10-19 23:07:07 +0530

    trans pose of matrix using arr

--- array_2D/transPose.cpp
+++ array_2D/transPose.cpp
@@ -1,17 +1,39 @@
 #include<iostream>
 using namespace std;
+void print(int arr[][4],int row, int column){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void Transpose(int arr[][4],int row,int column){
+    for(int i=0;i<row;i++){
+        for(int j=i;j<column;j++){
+
+            swap(arr[i][j], arr[j][i]);
+            
+        }
+    }
+}
 int main(){
     int arr[3][4] = {
                     {1,2,9,3},
                      {3,7,5,7},
                      {2,4,8,6}
                      };
-  // trans pose of matrix
- for(int i=0;i<4;i++){
-    
-    for(int j=0; j<3;j++){
-        cout<<arr[j][i]<<" ";
-    }
-    cout<<endl;
- }
+
+int row = 3;
+int column = 4;
+
+cout<<"Print before trans pose"<<endl;
+print(arr,row, column);
+
+
+cout<<"print after transpose"<<endl;
+Transpose(arr,row,column);
+print(arr,row, column);
+
 }
